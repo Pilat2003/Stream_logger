e2a1625538e1b8751965b8763a92b9ce878cbddf
Pilat2003 <mateuszpilat012003@gmail.com>
2023-03-01 11:43:28 +0100

    Update
    up to looped continue with

--- Program.cs
+++ Program.cs
@@ -7,53 +7,56 @@ namespace anakonda;
 class Program
 {
     public static bool Execute = true;
+    public static string message = "";
+    public static Action<Task<string>> ReadVSCode = (a)=>{
+        message+= "[VS Code] \n";
+            message+= a;
+             message+= "[*VS Code*] \n";
+             if(Execute)
+             inputRead.ReadLineAsync().ContinueWith(ReadVSCode);
+    };
+
+    public static Action<Task<string>> ReadOmni = (a)=>{
+        message+= "[Omnisharp] \n";
+            message+= a;
+             message+= "[*Omnisharp*] \n";
+             if(Execute)
+             r.ReadLineAsync().ContinueWith(ReadOmni);
+    };
+
+    public static StreamReader inputRead;
+    public static StreamReader r;
     static void Main(string[] args)
     {
         Stream input = Console.OpenStandardInput();
-        StreamReader inputRead = new StreamReader(input);
+        inputRead = new StreamReader(input);
         //Console.WriteLine("Hello, World!");
      Process process = new Process();
 // Configure the process using the StartInfo properties.
-process.StartInfo.FileName = @"C:\Users\Klasa1\.vscode\extensions\ms-dotnettools.csharp-1.25.4-win32-x64\.omnisharp\1.39.4-net6.0\omnisharp.exe";
+process.StartInfo.FileName = @"C:\Users\Komputer\.vscode\extensions\ms-dotnettools.csharp-1.25.4-win32-x64\.omnisharp\1.39.4-net6.0\omnisharp.exe";
 process.StartInfo.RedirectStandardInput = true;
 process.StartInfo.RedirectStandardOutput= true;
-        FileStream s = File.Open(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\data.txt", FileMode.OpenOrCreate);
-        s.SetLength(0);
-        StreamWriter w = new StreamWriter(s);
-
+        
 
         process.Start();
 
 
-        StreamReader r = new StreamReader(process.StandardOutput.BaseStream);
-        string message = "";
+        r = new StreamReader(process.StandardOutput.BaseStream);
+        message = "";
         message += "[ARGS] \n";
         foreach(string bfv in args){
         message += bfv + "\n";
         }
         message += "\n";
         
-        Task.Run(()=>{
+       r.ReadLineAsync().ContinueWith(ReadOmni);
+       inputRead.ReadLineAsync().ContinueWith(ReadVSCode);
+ 
             Thread.Sleep(10000);
             Execute = false;
-        });
-                while(Execute){
-            while(r.Peek() != -1){
-             message+= "[VS Code] \n";
-            message+= r.ReadLine();
-             message+= "[*VS Code*] \n";
-        }
-
-        while(r.Peek() != -1){
-             message+= "[Omni] \n";
-             string ss = inputRead.ReadLine();
-             System.Console.WriteLine(ss);
-            message+= ss;
-             message+= "[*Omni*] \n";
-        }
-       
-        
-        }
+            FileStream s = File.Open(@"F:\\data.txt", FileMode.OpenOrCreate);
+        s.SetLength(0);
+        StreamWriter w = new StreamWriter(s);
 
 
          w.WriteLine(message);
